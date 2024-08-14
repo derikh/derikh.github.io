f6c1b064a09b1aa68849bccc2a3e53be5136764b
derikh <derik.haun@gmail.com>
2024-08-14 00:27:50 -0600

    overhaul pt. 2

---
+++ settings.js
@@ -0,0 +1,13 @@
+// settings
+const speed = 1;
+
+const fluidTempoAccuracy = 4; // for ritardando, accelerando, etc. (idk if we need this)
+
+const yCorrectionConstant = (320/22.5)/14; // it's a common misconception that there are 14 steps between the sideline and hash; it's actually ~14.2. 🤓👆
+
+const zoomSensitivity = .1;
+const zoomLowerLimit = 1;
+const zoomUpperLimit = 50;
+
+const squareX = 1620;
+const squareY = 1280 + (16*22.5*yCorrectionConstant);
